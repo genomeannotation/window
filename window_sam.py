f8a1b396d28a1899c5d0b8b67bdf3a82b8116431
Theodore DeRego <tderego94@gmail.com>
2015-10-16 13:44:17 -1000

    window_sam.py skip option. Fixes #1

--- window_sam.py
+++ window_sam.py
@@ -8,9 +8,13 @@ import sys, argparse, math
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--size', '-s', help="Window size", 
-            type=int, required=True)
+    parser.add_argument('--window-size', '-w', help="Window size", type=int, required=True)
+    parser.add_argument('--skip', '-s', help="Length to skip between windows", type=int)
     args = parser.parse_args()
+    window_size = args.window_size
+    skip = 0
+    if args.skip:
+        skip = args.skip
     # Read sam file one line at a time
     for line in sys.stdin:
         fields = line.strip().split()
@@ -18,12 +22,14 @@ def main():
             continue
         seq_id = fields[2]
         position = int(fields[3])
-        window_number = int(math.ceil(float(position)/args.size)) 
-        window_id = seq_id + "_window_" + str(window_number)
-        window_position = position - (args.size * (window_number - 1))
-        fields[2] = window_id
-        fields[3] = str(window_position)
-        sys.stdout.write("\t".join(fields) + "\n")
+        window_number = int(math.floor(float(position)/(window_size+skip))) 
+        window_id = seq_id + "_window_" + str(window_number+1)
+        window_position = position - ((window_size+skip) * window_number)
+        if position < window_size:
+            # Only write the read if it's not in the skip zone
+            fields[2] = window_id
+            fields[3] = str(window_position)
+            sys.stdout.write("\t".join(fields) + "\n")
 
 
 ##########################
